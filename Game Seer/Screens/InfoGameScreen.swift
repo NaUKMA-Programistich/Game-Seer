72b66511145216753c4a6b34f04ab0251ff605af
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-06-09 20:41:10 +0300

    ML file and struct

---
+++ Game Seer/Screens/InfoGameScreen.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct InfoGameScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoGameScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoGameScreen()
+    }
+}
